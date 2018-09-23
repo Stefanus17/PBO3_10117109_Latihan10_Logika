15fa5e6aa2549e2dc7b82b8fad6695dc10162aff
Stefanus17 <43497735+Stefanus17@users.noreply.github.com>
2018-09-23 19:06:49 +0700

    Add files via upload

---
+++ PBO3_10117109_Latihan10_Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117109_latihan10_logika;
+
+/**
+ *
+ * @author 
+ * NAMA  : Stefanus Saputra
+ * KELAS : IF 3
+ * NIM   : 10117109
+ */
+public class PBO3_10117109_Latihan10_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
